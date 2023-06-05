6c2c41e733a00d52079e611559f18c1c3fe51918
yinlin <yinn.lin@mail.utoronto.ca>
2023-06-05 17:57:54 -0400

    Completed all 6 TODO tasks. YinLingithub.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i -= 1){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,6 +148,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] words = to_split.split(" ");
+        for (int i = 0; i <= (words.length - 1); i += 1){
+            ret.append(words[i].charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +179,9 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 1; i <= (arr.length - 1); i += 2){
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
